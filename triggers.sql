ee05ffba6d834864a6563b44a9caad064d8117f3
Tomás El Fakih <tomaselfakih@gmail.com>
2020-12-17 23:43:25 -0400

    Documentación de triggers. Cálculo de tiempo total de carrera, así como el número de vueltas totales

--- triggers.sql
+++ triggers.sql
@@ -35,23 +35,34 @@ execute function puesto_inicio();
 
 -- Función a ejecutar luego de la inserción de una participación, para generar
 -- las 24 horas de estadísticas
-create or replace function participar() returns trigger
+create or
+    replace function participar() returns trigger
     language plpgsql
 as
 $$
 declare
-    estadistica          estadistica; -- Auxiliar para la estadística que se genera cada hora
-    longitud_circuito    real; -- Longitud del circuito del evento
-    velocidad_vehiculo   real; -- Velocidad del vehículo del participante
-    umbral_falla_tecnica integer; -- Variable de control para las fallas técnicas
-    falla_tecnica        falla_tecnica; -- Auxiliar para fallas técnicas generadas
+    estadistica           estadistica; -- Auxiliar para la estadística que se genera cada hora
+    longitud_circuito     real; -- Longitud del circuito del evento
+    velocidad_vehiculo    real; -- Velocidad del vehículo del participante
+    umbral_falla_tecnica  integer; -- Variable de control para las fallas técnicas
+    falla_tecnica         falla_tecnica; -- Auxiliar para fallas técnicas generadas
 
+    -- usados al final
+    distancia_total       real; -- distancia total recorrida
+    velocidad_media_final real; -- Velocidad media al finalizar la carrera
+    horas_totales         integer; -- Tiempo total de competición
 begin
-    select longitud from evento where id = new.evento_id into longitud_circuito;
-    select velocidad from vehiculo where id = new.vehiculo_id into velocidad_vehiculo;
+    select e.longitud, v.velocidad
+    from evento e,
+         vehiculo v
+    where e.id = new.evento_id
+      and v.id = new.vehiculo_id
+    into longitud_circuito, velocidad_vehiculo;
     -- Inicializamos el umbral a 3. Cuando salga una falla técnica que afecte el tiempo.
     -- Mientras sea 0, podemos generar fallas técnicas, si no es 0, déjamos que no le pase nada en la carrera
     umbral_falla_tecnica := 0;
+    -- Generamos el equipo tecnico completo
+    new.equipo_tecnico := equipo_tecnico_completo();
     -- Iteramos por las 24 horas de la participación
     for i in 1..24
         loop
@@ -67,16 +78,18 @@ begin
                         declare
                             distancia real; -- distancia parcial recorrida
                         begin
+                            -- Aplicamos la penalización
                             estadistica.peor_tiempo_vuelta :=
                                     aplicar_penalizacion(falla_tecnica, estadistica.peor_tiempo_vuelta);
                         exception
+                            -- Si la penalización es grave, tenemos que forzar el abandono del participante
                             when sqlstate 'GRAVE' then
                                 distancia := distancia_recorrida(estadistica.vel_media);
                                 new.abandono :=
                                         row (falla_tecnica.parte_afectada, cantidad_vueltas(distancia, longitud_circuito));
                         end;
                         -- Atacamos el siguiente índice de las fallas técnicas
-                        new.fallas[num_fallas+1] := falla_tecnica; -- Independientemente de lo que pase, agregamos la falla técnica al conjunto
+                        new.fallas[num_fallas + 1] := falla_tecnica; -- Independientemente de lo que pase, agregamos la falla técnica al conjunto
                     end if;
                 end;
             else
@@ -88,9 +101,17 @@ begin
             exit when new.abandono is not null;
         end loop;
     if array_length(new.estadisticas_hora, 1) = 24 and
-       new.abandono is not null then -- la comprobación con not null es por si acaso falló en la última hora
+       new.abandono is null then -- la comprobación con null es por si acaso falló en la última hora
         new.finalizo := true;
     end if;
+    -- Calculamos el tiempo total
+    select sum((est).distancia_recorrida), avg((est).vel_media), count(*)
+    into distancia_total, velocidad_media_final, horas_totales
+    from unnest(new.estadisticas_hora) est;
+    new.tiempo_total := make_interval(hours := horas_totales) +
+                        calcular_tiempo_sobrante(cantidad_vueltas(distancia_total, longitud_circuito),
+                                                 velocidad_media_final, longitud_circuito);
+    new.numero_vueltas := cantidad_vueltas(distancia_total, longitud_circuito);
     return new;
 end;
 $$;
