c700473dab7aada4805ff49bacf1591084952bdf
Tomás El Fakih <tomaselfakih@gmail.com>
2021-02-02 10:48:05 -0400

    Añadido archivo inicial de seguridad

---
+++ Datamart/roles.sql
@@ -0,0 +1,24 @@
+create role Desarrollador superuser;
+create role "Analista de negocios 1";
+create role "Analista de negocios 2";
+create role "Analista de negocios 3";
+
+grant select ON ALL TABLES IN SCHEMA "postgres"."public" to "Analista de negocios 1";
+grant execute on function lemans_datamart.public.reporte_cinco_data_piloto(integer) to "Analista de negocios 2";
+grant execute on function lemans_datamart.public.reporte_cinco_data_participaciones(integer) to "Analista de negocios 2";
+grant execute on function lemans_datamart.public.reporte_seis_aplanar(integer) to "Analista de negocios 2";
+grant execute on function lemans_datamart.public.reporte_seis_data(integer) to "Analista de negocios 2";
+grant execute on function lemans_datamart.public.reporte_siete_data(integer) to "Analista de negocios 2";
+grant execute on function lemans_datamart.public.reporte_ocho_data(integer) to "Analista de negocios 2";
+grant execute on function lemans_datamart.public.reporte_nueve_contador(integer) to "Analista de negocios 2";
+grant execute on function lemans_datamart.public.reporte_nueve_data() to "Analista de negocios 2";
+grant execute on function lemans_datamart.public.reporte_diez_aux(integer) to "Analista de negocios 2";
+grant execute on function lemans_datamart.public.reporte_diez_data(integer) to "Analista de negocios 2";
+grant execute on function lemans_datamart.public.reporte_once_data(integer) to "Analista de negocios 3";
+grant execute on function lemans_datamart.public.reporte_doce_data(integer) to "Analista de negocios 3";
+grant execute on function lemans_datamart.public.reporte_trece() to "Analista de negocios 3";
+grant execute on function lemans_datamart.public.reporte_catorce() to "Analista de negocios 3";
+grant execute on function lemans_datamart.public.reporte_quince_autos(varchar) to "Analista de negocios 3";
+grant execute on function lemans_datamart.public.reporte_quince_cauchos(varchar) to "Analista de negocios 3";
+grant execute on function lemans_datamart.public.reporte_quince_cauchos(varchar) to "Analista de negocios 3";
+grant execute on function lemans_datamart.public.reporte_dieciseis_data_piloto(integer) to "Analista de negocios 3";
