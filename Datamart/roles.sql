c1a5fb83bf9ac8ba88de8cf52d97775eaf549014
Tomás El Fakih <tomaselfakih@gmail.com>
2021-02-02 20:55:03 -0400

    Script de roles realizado. Creación de script de instalación de funiones de reporte

--- Datamart/roles.sql
+++ Datamart/roles.sql
@@ -1,9 +1,16 @@
-create role Desarrollador superuser;
+-- Creación de roles
+create role "Desarrollador" superuser; -- == Admin
 create role "Analista de negocios 1";
 create role "Analista de negocios 2";
 create role "Analista de negocios 3";
 
-grant select ON ALL TABLES IN SCHEMA "postgres"."public" to "Analista de negocios 1";
+-- Asignación permisos para Analista 1
+grant connect on database postgres to "Analista de negocios 1";
+grant usage on schema public to "Analista de negocios 1";
+grant select on all tables in schema public to "Analista de negocios 1";
+
+-- Permisos para rol Analista 2
+grant execute on function lemans_datamart.public.reporte_cuatro_data(integer) to "Analista de negocios 2";
 grant execute on function lemans_datamart.public.reporte_cinco_data_piloto(integer) to "Analista de negocios 2";
 grant execute on function lemans_datamart.public.reporte_cinco_data_participaciones(integer) to "Analista de negocios 2";
 grant execute on function lemans_datamart.public.reporte_seis_aplanar(integer) to "Analista de negocios 2";
@@ -13,6 +20,8 @@ grant execute on function lemans_datamart.public.reporte_ocho_data(integer) to "
 grant execute on function lemans_datamart.public.reporte_nueve_contador(integer) to "Analista de negocios 2";
 grant execute on function lemans_datamart.public.reporte_nueve_data() to "Analista de negocios 2";
 grant execute on function lemans_datamart.public.reporte_diez_aux(integer) to "Analista de negocios 2";
+
+-- Permisos para rol Analista 3
 grant execute on function lemans_datamart.public.reporte_diez_data(integer) to "Analista de negocios 2";
 grant execute on function lemans_datamart.public.reporte_once_data(integer) to "Analista de negocios 3";
 grant execute on function lemans_datamart.public.reporte_doce_data(integer) to "Analista de negocios 3";
@@ -22,3 +31,11 @@ grant execute on function lemans_datamart.public.reporte_quince_autos(varchar) t
 grant execute on function lemans_datamart.public.reporte_quince_cauchos(varchar) to "Analista de negocios 3";
 grant execute on function lemans_datamart.public.reporte_quince_cauchos(varchar) to "Analista de negocios 3";
 grant execute on function lemans_datamart.public.reporte_dieciseis_data_piloto(integer) to "Analista de negocios 3";
+
+-- Creación de usuarios
+create user dev in role Desarrollador password 'dev1';
+create user analista1 in role "Analista de negocios 1" password 'anal1sta';
+create user analista2 in role "Analista de negocios 2" password 'analista2';
+create user analista3 in role "Analista de negocios 3" password '3lanalista';
+
+
---
+++ reportes/E3/instalar.sql
@@ -0,0 +1,16 @@
+-- Archivo utilitario para instalar todas las funciones de los reportes de manera rápida
+
+\i './auxiliar.sql';
+\i './reporte_cuatro.sql';
+\i './reporte_cinco.sql';
+\i './reporte_seis.sql';
+\i './reporte_siete.sql';
+\i './reporte_ocho.sql';
+\i './reporte_nueve.sql';
+\i './reporte_diez.sql';
+\i './reporteonce.sql';
+\i './reporte_doce.sql';
+\i './reporte_trece.sql';
+\i './reporte_catorce.sql';
+\i './reporte_quince.sql';
+\i './reporte_diesiceis.sql';
